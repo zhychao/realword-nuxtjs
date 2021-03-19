2613ee452dc00fd684a129955118a3087997af5d
zhangyc <zhangyc@cgbchina.com>
2021-03-19 10:49:16 +0800

    配置文件路径错误，修改路径

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
   <div class="banner">
     <div class="container">
-      <h1 class="logo-font">拉钩教育2021/3/19改 test conduit</h1>
+      <h1 class="logo-font">拉钩教育2021/3/19 www改 test conduit</h1>
       <p>A place to share your knowledge.</p>
     </div>
   </div>
